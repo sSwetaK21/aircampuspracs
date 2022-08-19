2a038b06c7a96aff1a55c698c4fd1bc36878cb62
shweta <shwetakodamg@gmail.com>
2022-08-19 22:43:28 +0530

    project

--- practices.js
+++ practices.js
@@ -12,11 +12,22 @@
 //   }
 //   setInterval(func,1000)
 
-const print = (n) => {
-    for (let i = 1; i <= n; i++) {
-      setTimeout(() => {
-        console.log(i)
-      }, i * 1000)
-    }
-  }
-  print(10);
+// const print = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//       setTimeout(() => {
+//         console.log(i)
+//       }, i * 1000)
+//     }
+//   }
+//   print(10);
+
+const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+add();
+add();
+add();
+
+// the counter is now 3
