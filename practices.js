02993e6556b7f339f59f4054e3d866a16d982dac
shweta <shwetakodamg@gmail.com>
2022-08-19 22:41:55 +0530

    project

--- practices.js
+++ practices.js
@@ -1,7 +1,22 @@
-let setclear = setInterval(function (){
-    for(let i=0;i<5;i++){
-        console.log(i)
-    }
-},1000)
+//  setInterval(function add(){
+//     for(let i=0;i<5;i++){
+//         console.log(i)
+//     }
+// },1000)
 
-clearInterval(setclear)
+// clearInterval(setclear)
+
+
+// function func(){
+//     console.log("Run")
+//   }
+//   setInterval(func,1000)
+
+const print = (n) => {
+    for (let i = 1; i <= n; i++) {
+      setTimeout(() => {
+        console.log(i)
+      }, i * 1000)
+    }
+  }
+  print(10);
